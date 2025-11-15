180ec31dd6868d4f8f80267612c59ccf9ab9fe46
margihingrajia <69033000+margihingrajia@users.noreply.github.com>
2025-11-15 08:33:36 -0500

    Time: 8 ms (23.04%) | Memory: 25.8 MB (9.24%) - LeetSync

---
+++ Database/36-valid-sudoku/valid-sudoku.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<char> row[9], col[9], box[9];
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0; j<9; j++)
+            {
+                char c = board[i][j];
+                if (c=='.') continue;
+                int idx = ((i/3) *3 + (j/3));
+                if (row[i].count(c)||col[j].count(c)||box[idx].count(c)) return false;
+                row[i].insert(c);
+                col[j].insert(c);
+                box[idx].insert(c);
+            }
+        }
+        return true;
+    }
+};
